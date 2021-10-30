bc47bc089e9b82f358d63fd20035f7c543031394
Maddy <maddy.kilsbymcmurray@gmail.com>
2021-10-30 10:35:16 +0100

    service updated

--- AdoptingService/src/main/java/com/ironhack/adoptingservice/AdoptingController.java
+++ AdoptingService/src/main/java/com/ironhack/adoptingservice/AdoptingController.java
@@ -2,11 +2,10 @@ package com.ironhack.adoptingservice;
 
 import com.ironhack.adoptingservice.dto.AdoptedDTO;
 import com.ironhack.adoptingservice.dto.AdopterDTO;
+import com.ironhack.adoptingservice.dto.AdopterRequestDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class AdoptingController {
@@ -14,9 +13,9 @@ public class AdoptingController {
     @Autowired
     AdoptingService adoptingService;
 
-    @GetMapping("/adopt")
+    @GetMapping("/adopt/{id}")
     @ResponseStatus(HttpStatus.OK)
-    public AdoptedDTO adoptAPet() {
-
+    public AdoptedDTO adoptAPet(@PathVariable(name = "id") Long id, @RequestBody AdopterRequestDTO adopterRequestDTO) {
+        return adoptingService.adoptAPet(id, adopterRequestDTO);
     }
 }
