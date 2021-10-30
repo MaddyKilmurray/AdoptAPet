dacf904eb4861386b8cd2c4196a642a7e1ffb0aa
Maddy <maddy.kilsbymcmurray@gmail.com>
2021-10-30 08:50:54 +0100

    services added

---
+++ AdoptingService/src/main/java/com/ironhack/adoptingservice/AdoptingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ironhack.adoptingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdoptingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdoptingServiceApplication.class, args);
+    }
+
+}
---
+++ AdoptingService/src/test/java/com/ironhack/adoptingservice/AdoptingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ironhack.adoptingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdoptingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
